c0e8f29cba6533243d2a20bc526164297bf3738c
MohaimenulHaque <93148911+MohaimenulHaque@users.noreply.github.com>
2025-05-16 23:33:53 +0600

    Add files via upload

---
+++ HomePage.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Desco Website</title>
+    <link rel="icon" href="desco.png" type="image">
+    <link rel="stylesheet" href="homepage.css">
+</head>
+
+<body>
+
+    <div class="header">
+
+        <h1> Online Customer Service Management System</h1>
+
+        <div class="logo">
+            <img src="desco2.png" alt="Desco Logo">
+        </div>
+
+        <nav>
+            <a href="#">Home</a>
+            <a href="https://desco.gov.bd/" target="_blank">FAQ</a>
+            <a href="https://desco.gov.bd/" target="_blank">Charge</a>
+            <a href="https://desco.gov.bd/" target="_blank">Information Desk</a>
+            <a href="login.php">Customer Login</a>
+        </nav>
+    </div>
+
+
+    <div class="container">
+
+        <a href="login.php">
+            <div class="box">
+                <img src="img/img1.jpg" alt="">
+                <h2>Customer</h2>
+            </div>
+        </a>
+        <a href="adminlogin.php">
+            <div class="box">
+                <img src="img/img2.jpg" alt="">
+                <h2>Admin</h2>
+            </div>
+        </a>
+
+    </div>
+
+
+    <div class="text">
+        <p> Developed and maintained by NSU students </p>
+    </div>
+
+</body>
+
+</html>
